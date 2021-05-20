deb738883766b83c2de200c92443633b85fda95d
thuanbasto <thuanbasto113@gmail.com>
2021-05-20 16:36:30 +0700

    fix Assignment 802

--- JPE Assignment/src/assignment_802/services/BookService.java
+++ JPE Assignment/src/assignment_802/services/BookService.java
@@ -1,5 +1,8 @@
 package assignment_802.services;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -34,14 +37,25 @@ public class BookService {
 		System.out.println("Enter the string to search: ");
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
+		List<Book> sortedList = new ArrayList<>();
 		
-		System.out.println(" ------ List of book -------");
 		for (Publication publication : list) {
 			if (publication instanceof Book) {
 				Book book = (Book) publication;
 				String str = book.getIsbn() + " " + book.getAuthor() + " " + book.getPublisher();
-				if (str.contains(input)) System.out.println(book);
+				if (str.contains(input)) sortedList.add(book);
 			}
 		}
+		
+		Collections.sort(sortedList, new Comparator<Book>() {
+			@Override
+			public int compare(Book o1, Book o2) {
+				if (o1.isbn.compareTo(o2.isbn) == 0) 
+					return o1.publicationDate.compareTo(o2.publicationDate);
+				return o1.isbn.compareTo(o2.isbn);
+			}
+		});
+		System.out.println(" ------ List of book -------");
+		sortedList.forEach(book -> System.out.println(book));
 	}
 }
